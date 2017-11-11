6a257cf2044a103d11603efd54c4a5d313a1cd46
radicaleinward <32418110+radicaleinward@users.noreply.github.com>
2017-11-10 19:16:04 -0500

    bug fix
    Did not put spaces after rbg selection, did not === with the pickedColor var.

--- projects/colorGame/colorGame.js
+++ projects/colorGame/colorGame.js
@@ -44,5 +44,5 @@ function randomColor(){
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
-	return "rgb(" + r + "," + g + "," + b + ")";
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
