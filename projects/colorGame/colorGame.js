f741e238e45caea074653f24b1fc5befd6166d62
radicaleinward <32418110+radicaleinward@users.noreply.github.com>
2017-11-10 19:33:56 -0500

    updates

--- projects/colorGame/colorGame.js
+++ projects/colorGame/colorGame.js
@@ -3,6 +3,17 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = colors[i];
+	}
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -13,6 +24,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
 			changeColors(pickedColor);
+			h1.style.backgroundColor = clickedColor;
 		}
 		else {
 			this.style.background = "#232323";
