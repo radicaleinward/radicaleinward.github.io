9e4a0ac77388770e530a51917cd1bfcbddbaaa4e
radicaleinward <32418110+radicaleinward@users.noreply.github.com>
2017-11-10 19:04:47 -0500

    Color Game Prototype
    Early stages of color-picking game.

--- projects/colorGame/colorGame.js
+++ projects/colorGame/colorGame.js
@@ -1,27 +1,48 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++) {
+for(var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.backgroundColor;
 		if(clickedColor === pickedColor){
-			alert("Correct!");
+			messageDisplay.textContent = "Correct!";
+			changeColors(pickedColor);
 		}
 		else {
-			alert("Wrong!");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 		});
 }
 
+function changeColors(color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	var arr = []
+	for(var i = 0; i < num; i++){
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + "," + g + "," + b + ")";
+}
