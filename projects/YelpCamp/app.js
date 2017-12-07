96295764e8515b96b65930a5d37d2eae5e695c56
radicaleinward <32418110+radicaleinward@users.noreply.github.com>
2017-12-07 15:52:03 -0500

    Refactored & added data persistence.
    Set up database with mongoose.

--- projects/YelpCamp/app.js
+++ projects/YelpCamp/app.js
@@ -1,38 +1,49 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-    {name: "Granite Hill", image: "https://farm3.staticflickr.com/2927/14442300701_7952568539.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
-];
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
-app.use(bodyParser.urlencoded({extended: true}));  //no explanation as to the specifics of the code, other than "memorize it or copy/paste"
-                                                   // just get used to having it in our code
-app.set("view engine", "ejs"); // this makes it so we don't need to type out .ejs at the end of rendered files
+mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
-
-
-app.get("/", function(req, res){
-    res.render("landing");  // technically landing.ejs - we installed ejs as the engine viewer, so no need for file extensions here
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String
 });
 
-app.get("/campgrounds", function(req, res){;
-    res.render("campgrounds",{campgrounds:campgrounds}); // Passing data called "campgrounds" - we can call it anything. See
-                                                         // line 3 on the campgrounds.ejs file. The left hand side name "campgrounds"
-                                                         // can say anything, but the right hand side is the variable we named.
-                                                         // at the time of writing this comment, there were three items in the array,
-                                                         // and loading the page showed [object, Object] x 3.
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+           res.render("campgrounds",{campgrounds:allCampgrounds});
+       }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
+    
     var name = req.body.name; // pulls (REQuests) the namne from the POST that we're inside and named "name" in the new.ejs file
     var image = req.body.image;  // pulls the image from the POST we are inside and named "image" in the new.ejs file
-    var newCampground = {name: name, image: image}; // {} is an object
-    campgrounds.push(newCampground);  // push function sends new campground object(s) (see line above) to campgrounds page
-    res.redirect("/campgrounds"); // sends user back to campgrounds GET page. GET is default for redirect requests
-});  //same ejs name but DIFFERENT page because it's a POST vs a GET - best practice.  Post request
-                            //should have the same name as the get request page.
+    var newCampground = {name: name, image: image} // {} is an object
+    
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            
+            res.redirect("/campgrounds");
+        }
+    });
+});
                             
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
@@ -41,4 +52,3 @@ app.get("/campgrounds/new", function(req, res){
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server is On");
 });
-
