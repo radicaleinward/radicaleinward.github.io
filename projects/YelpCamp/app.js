359fc0e5448618d00588bb97d6746a9c16f0ceb8
radicaleinward <32418110+radicaleinward@users.noreply.github.com>
2017-12-13 09:03:26 -0500

    Refactored various pages.
    Refactored nav bar to clean up code and make it more simplistic.  Will spice it up a bit later, looking for it to be clean and functional first.

--- projects/YelpCamp/app.js
+++ projects/YelpCamp/app.js
@@ -9,10 +9,28 @@ app.set("view engine", "ejs");
 
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   image: String
+   image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+    // {
+    //     name:"Granite Hill",
+    //     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+    //     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite."
+        
+    // },
+    // function(err, campground){
+    //     if(err){
+    //         console.log(err);
+    //     } else {
+    //         console.log("NEWLY CREATED CAMPGROUND:  ");
+    //         console.log(campground);
+    //     }
+    // });
+
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -24,7 +42,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-           res.render("campgrounds",{campgrounds:allCampgrounds});
+           res.render("index",{campgrounds:allCampgrounds});
        }
     });
 });
@@ -33,7 +51,9 @@ app.post("/campgrounds", function(req, res){
     
     var name = req.body.name; // pulls (REQuests) the namne from the POST that we're inside and named "name" in the new.ejs file
     var image = req.body.image;  // pulls the image from the POST we are inside and named "image" in the new.ejs file
-    var newCampground = {name: name, image: image} // {} is an object
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc} // {} is an object
+  
     
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -47,6 +67,17 @@ app.post("/campgrounds", function(req, res){
                             
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+       if(err){
+           console.log(err);
+       } else {
+           res.render("show", {campground: foundCampground});
+       }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
